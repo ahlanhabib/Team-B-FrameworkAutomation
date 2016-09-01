bd8370d66e782a91402878d260d21659a94dbff2
Nahreen <nahreen789@gmail.com>
2016-09-01 08:48:24 -0400

    +SearchItem

--- Amazon/src/test/java/Search/amazon/SearchItem.java
+++ Amazon/src/test/java/Search/amazon/SearchItem.java
@@ -1,7 +1,28 @@
 package Search.amazon;
 
+import Common.Base;
+import org.openqa.selenium.Keys;
+import org.testng.annotations.Test;
+
 /**
  * Created by AFMErshadul on 8/29/2016.
  */
-public class SearchItem {
+public class SearchItem extends Base{
+    @Test
+    public void searchItems() throws InterruptedException {
+
+        //Test-7: searching for laptop from search button
+        typeByCss("#twotabsearchtextbox", "Laptop");
+        sleepFor(5);
+        clickByCss(".nav-input");
+        clickByXpath(".//*[@id='result_1']/div/div[2]/div/div/div/div/a/div/div/span");
+        sleepFor(5);
+        navigateBack();
+        navigateBack();
+        //scrollToElementByxpath(".//*[@id='nav-link-shopall']/span[2]");
+        sleepFor(20);
+
+
+
+    }
 }
